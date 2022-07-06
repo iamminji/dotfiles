c83c7ceea02d8301a9dcd10072237466e72cbd33
iamminji <minji.dev@gmail.com>
2022-07-06 22:57:44 +0900

    starship 사용 추가

--- setup-mac.sh
+++ setup-mac.sh
@@ -11,3 +11,7 @@ rustup component add rustfmt
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sed -i '' 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' ~/.zshrc
 
+# new bash
+curl -sS https://starship.rs/install.sh | sh
+echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+
